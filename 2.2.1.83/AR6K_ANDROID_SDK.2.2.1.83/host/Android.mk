2b5fc534887336723ec26c323e379cdde0cbb624
Aisheng.Dong <b29396@freescale.com>
2010-06-10 13:48:06 +0800

    Fix unable to copy ar6000.ko to /system/lib/modules by Android.mk for android.
    The directory  /system/lib/modules should be created first
    before the copy operation or the copy will fail.
    
    Signed-off-by: Aisheng.Dong <b29396@freescale.com>

--- 2.2.1.83/AR6K_ANDROID_SDK.2.2.1.83/host/Android.mk
+++ 2.2.1.83/AR6K_ANDROID_SDK.2.2.1.83/host/Android.mk
@@ -24,6 +24,7 @@ mod_file := $(TARGET_OUT)/lib/modules/ar6000.ko
 $(mod_file) :
 	cd $(ATH_SRC) && ./comp.sh 2 ${PWD}/$(ATH_LINUXPATH)
 	cd $(ATH_SRC) && ./comp.sh 1 ${PWD}/$(ATH_LINUXPATH)
+	mkdir -p $(TARGET_OUT)/lib/modules
 #	$(MAKE) ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- -C $(ATH_LINUXPATH) ATH_HIF_TYPE=$(ATH_HIF_TYPE) SUBDIRS=${PWD}/$(ATH_SRC)/os/linux modules
 	$(ACP) $(ATH_SRC)/os/linux/ar6000.ko $(TARGET_OUT)/lib/modules/
 
